ba04b8032999be3a219fb88d25da8bfaf640efb2
Crevi <crevi@outlook.com>
2015-05-21 13:30:35 +0200

    created a directory called 'site_details' by running startapp in the terminal. Added site_details to settings.py under INSTALLED_APPS.

--- appen/theme/urls.py
+++ appen/theme/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from theme import views
+from django.conf.urls import include, patterns, url
+from theme import views 
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)), 
-    url(r'^$', views.frontpage, name='frontpage')
+urlpatterns = patterns('', 
+	url(r'^$', views.frontpage, name='frontpage'),
 )
