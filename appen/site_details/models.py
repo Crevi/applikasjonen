8e4b767f6ad0ddfb747bf837b250a7ae25152f63
Crevi <crevi@outlook.com>
2015-05-27 15:55:58 +0200

    Emptied out models.py in 'users'directory, and added content to models.py in 'site_details' directory (a 'User' class and a unicode function).

--- appen/site_details/models.py
+++ appen/site_details/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model): 
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=100)
+	username = models.CharField(max_length=50)
+	email = models.EmailField()
+
+def __unicode__(self): 
+	return u'%s %s' (self.first_name, self.last_name)
--- appen/users/models.py
+++ appen/users/models.py
@@ -1,8 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import User
 # Create your models here.
-class User(models.Model): 
-	firstname = models.CharField(max_length=50)
-	lastname = models.CharField(max_length=100)
-	username = models.CharField(max_length=50)
-	mail = models.EmailField()
