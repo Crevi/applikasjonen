bc6b419d3990439e483fa1d29115b2eb7d4fa9ae
Crevi <crevi@outlook.com>
2015-05-28 12:15:54 +0200

    Made a 'templates' folder within 'site_details' directory, and created an account_settings.html and a base.html, with base as an extension of theme/site_base.html and account_settings an  extension of base. Account_settings.html includes a list of settings for the logged in user. Made a view for account_settings.html (showing the template), and linked to this view in urls.py. Then uncommented the line linking to site details urls.py in the urls.py in 'appen' (root directory). In base.html in 'theme' I simply added a url to the link in the navbar, so that the user has a link to account settings on the frontpage. NB note that the commented out lines in site_details/urls.py are just possible solutions to a NoReverseMatch error that appeared but is now fixed.

--- appen/site_details/urls.py
+++ appen/site_details/urls.py
@@ -1,2 +1,10 @@
-from django.conf.urls import patterns, url 
+from django.conf.urls import include, patterns, url
+from site_details import views 
+
+#from django.views.generic import TemplateView
+#from django.core.urlresolvers import reverse
+
+urlpatterns = patterns('', 
+	url(r'^$', views.account_settings, name='account_settings'), 
+)
 
